d0a976b8e0c313e3fba8deb790d37c7d657d9a02
Callum Taylor <callum@scrapprecycling.com>
2024-11-20 20:40:58 +0000

    Parsing data with json.

--- backend/app/helpers/get_street_names.py
+++ backend/app/helpers/get_street_names.py
@@ -1,18 +1,21 @@
 from councils.edinburgh import get_possible_addresses_by_letter
+import json
 
 def get_edinburgh_streets():
     alphabet = list(map(chr, range(97, 123)))
     page_num = 1
-
-    with open('data/edin_streets.csv', 'w', newline='') as csvfile:
+    output_data = {}
+                
+    with open("data/edin_streets.json", "w", encoding="utf-8") as jsonfile: 
         for letter in alphabet:
+            page_num = 1
+            output_data[letter] = []  # Initialize an empty list for each letter
             while True:
                 streets = get_possible_addresses_by_letter(letter, page_num)
-                if streets != None:
-                    for street in streets:
-                        csvfile.write(street + '\n')
-                if streets == None:
-                    page_num = 1
-                    break
-                print(streets)
+                if streets is not None:
+                    output_data[letter].extend(streets)  # Add streets to the list
+                else:
+                    break  # Exit the loop if no more streets are found
                 page_num += 1
+        
+        json.dump(output_data, jsonfile, ensure_ascii=False, indent=4)  # Write to JSON file
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Papa from "papaparse";
 
 type BinColorMapping = {
   [key: string]: "green" | "grey";
@@ -7,39 +8,79 @@ type BinColorMapping = {
 function App() {
   // State to store the fetched data
   const [calendar, setCalendar] = useState<BinColorMapping | null>(null);
-  // Fetch data from FastAPI on component mount
+  const [edinStreets, setEdinStreets] = useState<string[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [selectedStreet, setSelectedStreet] = useState<string | null>(null);
+
+  const getEdinStreetNames = () => {
+    Papa.parse<string[]>(
+      "https://raw.githubusercontent.com/taybluetooth/scotbin/main/backend/app/data/edin_streets.csv",
+      {
+        download: true,
+        header: false,
+        skipEmptyLines: true,
+        complete: (results) => {
+          const streets = results.data.map((row) => row[0]);
+          const streetsSet = Array.from(new Set(streets));
+          setEdinStreets(streetsSet);
+        },
+        error: (error) => console.error("Error downloading CSV:", error),
+      }
+    );
+  };
+
+  const onChange = (e: React.FormEvent<HTMLSelectElement>) => {
+    setLoading(true);
+    setSelectedStreet(e.currentTarget.value.replace(" ", "%20"));
+  };
+
   useEffect(() => {
-    fetch("http://localhost:8000/calendar/edinburgh/Provost%20Haugh") // FastAPI endpoint URL
-      .then((response) => response.json()) // Parse the JSON response
-      .then((data) => setCalendar(JSON.parse(data))) // Set the data in the state
+    getEdinStreetNames();
+  }, []);
+
+  useEffect(() => {
+    if (!selectedStreet) return;
+
+    fetch(`http://localhost:8000/calendar/edinburgh/${selectedStreet}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setCalendar(JSON.parse(data));
+        setLoading(false);
+      })
       .catch((error) => console.error("Error fetching data: ", error));
-  }, []); // Empty dependency array, so it runs only once on mount
+  }, [selectedStreet]);
 
   return (
     <div className="App">
-      <h1>FastAPI + React Example</h1>
-
-      {calendar ? (
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <th>Date</th>
-                <th>Color</th>
-              </tr>
-            </thead>
-            <tbody>
+      <h1>Scotbin</h1>
+      <label htmlFor="edinburgh-street-dropdown">Select a Street:</label>
+      <select id="edinburgh-street-dropdown" onChange={onChange}>
+        <option value="">Select a street</option>
+        {edinStreets.map((street) => (
+          <option key={`edinburgh-street-${street}`} value={street}>
+            {street}
+          </option>
+        ))}
+      </select>
+      {!calendar && !loading && <p>Please select a street</p>}
+      {loading && <p>Loading data...</p>}
+      {!loading && calendar && (
+        <table>
+          <thead>
+            <tr>
+              <th>Dates</th>
+              <th>Bin</th>
+            </tr>
+          </thead>
+          <tbody>
             {Object.entries(calendar).map(([date, color]) => (
-                <tr key={date}>
-                  <td>{date}</td>
-                  <td>{color}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <p>Loading data...</p>
+              <tr key={date}>
+                <td>{date}</td>
+                <td>{color}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       )}
     </div>
   );
